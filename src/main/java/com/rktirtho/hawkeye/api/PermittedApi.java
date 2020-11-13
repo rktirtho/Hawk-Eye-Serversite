2d89b28f5b3762ea264f4c7622f07573801b5a8c
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-11-13 23:59:19 +0600

    get permitted by id done

--- src/main/java/com/rktirtho/hawkeye/api/PermittedApi.java
+++ src/main/java/com/rktirtho/hawkeye/api/PermittedApi.java
@@ -2,6 +2,7 @@ package com.rktirtho.hawkeye.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class PermittedApi {
 	@GetMapping("permitteds")
 	public List<Permitted> getAll(){
 		return repo.findAll();
+		
+	}
+	@GetMapping("permitted/{id}")
+	public Permitted getOne(@PathVariable("id") int id){
+		return repo.findById(id).orElse(null);
 		
 	}
 
