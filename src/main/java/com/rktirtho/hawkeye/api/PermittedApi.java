a4e0c48b58ee54a7479d58c25720afc8ad404b20
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-11-13 23:56:59 +0600

    getting all permitted person api done

--- src/main/java/com/rktirtho/hawkeye/api/PermittedApi.java
+++ src/main/java/com/rktirtho/hawkeye/api/PermittedApi.java
@@ -1,10 +1,26 @@
 package com.rktirtho.hawkeye.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rktirtho.hawkeye.model.Permitted;
+import com.rktirtho.hawkeye.repository.PermittRepo;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api")
 public class PermittedApi {
+	
+	@Autowired
+	PermittRepo repo;
+	
+	@GetMapping("permitteds")
+	public List<Permitted> getAll(){
+		return repo.findAll();
+		
+	}
 
 }
