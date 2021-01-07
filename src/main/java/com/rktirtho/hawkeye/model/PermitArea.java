d9b02bf7843fda618eadafc1bd130cdb9af9399b
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-07 21:46:39 +0600

    PermitArea model class defined with JPA annotation

--- src/main/java/com/rktirtho/hawkeye/model/PermitArea.java
+++ src/main/java/com/rktirtho/hawkeye/model/PermitArea.java
@@ -1,5 +1,39 @@
 package com.rktirtho.hawkeye.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class PermitArea {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private int personId;
+	private String area;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPersonId() {
+		return personId;
+	}
+	public void setPersonId(int personId) {
+		this.personId = personId;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+	
 
 }
