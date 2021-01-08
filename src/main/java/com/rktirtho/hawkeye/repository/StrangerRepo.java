881f0676b555a6bfe20d2584c26984bd7a27faf4
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-08 23:05:30 +0600

    Stranger Reposiroty created

---
+++ src/main/java/com/rktirtho/hawkeye/repository/StrangerRepo.java
@@ -0,0 +1,9 @@
+package com.rktirtho.hawkeye.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rktirtho.hawkeye.model.Stranger;
+
+public interface StrangerRepo extends JpaRepository<Stranger, Integer>{
+
+}
