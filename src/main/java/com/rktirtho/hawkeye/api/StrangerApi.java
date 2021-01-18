5caa3a0a56cbd397df20147736db55e67141a639
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-18 22:47:17 +0600

    find all stranger api added

--- src/main/java/com/rktirtho/hawkeye/api/StrangerApi.java
+++ src/main/java/com/rktirtho/hawkeye/api/StrangerApi.java
@@ -1,10 +1,27 @@
 package com.rktirtho.hawkeye.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rktirtho.hawkeye.model.Stranger;
+import com.rktirtho.hawkeye.repository.StrangerRepo;
+
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/stranger")
 public class StrangerApi {
+	
+	@Autowired
+	StrangerRepo repo;
+	
+	@GetMapping("all")
+	public List<Stranger> findAll(){
+		return repo.findAll();
+	}
+	
+	
 
 }
--- src/main/java/com/rktirtho/hawkeye/repository/StrangerRepo.java
+++ src/main/java/com/rktirtho/hawkeye/repository/StrangerRepo.java
@@ -1,9 +1,11 @@
 package com.rktirtho.hawkeye.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import com.rktirtho.hawkeye.model.Stranger;
 
+@Component
 public interface StrangerRepo extends JpaRepository<Stranger, Integer>{
 
 }
